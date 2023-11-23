09a7f103e315daa6d83185e037685928a450df8a
Maximiliano Gomez <alexander34223@gmail.com>
2023-11-23 05:29:56 -0300

    estructura completa

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const inputDeTarea = document.getElementById("inpunt-tarea");
+    const botonAgregar = document.getElementById("btn-agregar");
+    const listados = document.getElementById("List");
+
+    botonAgregar.addEventListener("click", () => {
+      let tarea = inputDeTarea.value;
+      if (tarea !== "") {
+        let listItem = document.createElement("li");
+        listItem.className = "list-group-item d-flex justify-content-between align-items-center";
+        listItem.innerHTML = ` La Tarea Igresada de ${tarea} <br>  Desea eliminarla ? <button class="btn btn-danger btn-sm quitar">Eliminar</button>`;
+        listados.appendChild(listItem);
+        inputDeTarea.value = "";
+      }
+    });
+
+    //  use una delegacion de evento
+    listados.addEventListener("click", (event) => {
+      if (event.target && event.target.matches(".quitar")) {
+        event.target.closest("li").remove();
+      }
+    });
